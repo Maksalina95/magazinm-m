0729bac5c2117c7f3f1163188d94d9a7bc939516
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-04 15:07:34 +0300

    Update config.js
    o

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const sheetId = '1fXTvbglK1iQ_HyTo6BKWw2EaSn79rnxm977bjNR_-0U';
-const url = https://opensheet.elk.sh/${sheetId}/Sheet1;
+const url = `https://opensheet.elk.sh/${sheetId}/Sheet1`;
 
 let products = [];
 let filteredProducts = [];
